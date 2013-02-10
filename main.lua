8e32648c1cded197efa2db1437dcb2e653684660
TJ Kells <tjkells@gmail.com>
2013-02-09 21:24:20 -0600

    add scrolling controls for debugging

--- main.lua
+++ main.lua
@@ -1,43 +1,79 @@
 require('lib/middleclass/middleclass')
-
 Map = require "map"
 
-screenWidth = MOAIEnvironment.screenWidth
-screenHeight = MOAIEnvironment.screenHeight
-
-if screenWidth == nil then screenWidth = 960 end
-if screenHeight == nil then screenHeight = 640 end
-
-MOAISim.openWindow("Window",screenWidth,screenHeight)
-
-viewport = MOAIViewport.new()
-viewport:setSize(screenWidth,screenHeight)
-viewport:setScale(screenWidth,screenHeight)
-
-mapLayer = MOAILayer2D.new()
-mapLayer:setViewport(viewport)
-MOAISim.pushRenderPass(mapLayer)
-
-tableauLayer = MOAILayer2D.new()
-tableauLayer:setViewport(viewport)
-MOAISim.pushRenderPass(tableauLayer)
-
-chars = 'MAPTBLUYER'
-
 font = MOAIFont.new()
-font:loadFromTTF('assets/arialbd.ttf',chars,120,72)
+font:loadFromTTF('assets/arialbd.ttf',"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",120,72)
 
---Draw some tiles
+screen_width = MOAIEnvironment.screen_width
+screen_height = MOAIEnvironment.screen_height
 
-myMap = Map:new(5,"wedge",mapLayer)
+if screen_width == nil then screen_width = 960 end
+if screen_height == nil then screen_height = 640 end
 
-tableauText = MOAITextBox.new()
-tableauText:setString('Tableau')
-tableauText:setFont(font)
-tableauText:setTextSize(120,72)
-tableauText:setYFlip(true)
-tableauText:setRect(-490,-(screenHeight+120),400,300)
-tableauText:setAlignment(MOAITextBox.CENTER_JUSTIFY,MOAITextBox.CENTER_JUSTIFY)
-tableauText:spool()
+MOAISim.openWindow("Wizard Soldier v0.01",screen_width,screen_height)
 
-mapLayer:insertProp(tableauText)
+map_viewport = MOAIViewport.new()
+map_viewport:setSize(screen_width,screen_height)
+map_viewport:setScale(screen_width,screen_height)
+
+tableau_viewport = MOAIViewport.new()
+tableau_viewport:setSize(screen_width,screen_height*0.10)
+tableau_viewport:setScale(screen_width,screen_height*0.10)
+
+map_layer = MOAILayer2D.new()
+map_layer:setViewport(map_viewport)
+
+tableau_layer = MOAILayer2D.new()
+tableau_layer:setViewport(tableau_viewport)
+
+layers = {}
+table.insert(layers, map_layer)
+table.insert(layers, tableau_layer)
+
+
+MOAIRenderMgr.setRenderTable(layers)
+
+myMap = Map:new(5,"wedge",map_layer)
+
+-- myQuad = MOAIGfxQuad2D.new()
+-- myQuad:setTexture("assets/images/turtle.png")
+-- myQuad:setRect(-25, -25, 25, 25)
+
+-- myImage = MOAIProp2D.new()
+-- myImage:setDeck(myQuad)
+-- myImage:setLoc(0,0)
+tableau_layer:insertProp(myImage)
+
+tableau_text = MOAITextBox.new()
+tableau_text:setString('Tableau')
+tableau_text:setFont(font)
+tableau_text:setTextSize(120,72)
+tableau_text:setRect(0,200,640,-300)
+tableau_text:setAlignment(MOAITextBox.CENTER_JUSTIFY,MOAITextBox.CENTER_JUSTIFY)
+tableau_text:spool()
+
+tableau_layer:insertProp(tableau_text)
+
+map_viewport.offsetX = 0
+map_viewport.offsetY = 0
+MOAIInputMgr.device.keyboard:setCallback(
+    function(key,down)
+        if down==true then
+        	if string.char(tostring(key)) == 'w' then
+        		map_viewport.offsetY = map_viewport.offsetY + 0.2
+        		map_viewport:setOffset(map_viewport.offsetX,map_viewport.offsetY)
+        	elseif string.char(tostring(key)) == 'a' then
+        		map_viewport.offsetX = map_viewport.offsetX - 0.2
+        		map_viewport:setOffset(map_viewport.offsetX,map_viewport.offsetY)
+        	elseif string.char(tostring(key)) == 's' then
+        		map_viewport.offsetY = map_viewport.offsetY - 0.2
+        		map_viewport:setOffset(map_viewport.offsetX,map_viewport.offsetY)
+        	elseif string.char(tostring(key)) == 'd' then
+        		map_viewport.offsetX = map_viewport.offsetX + 0.2
+        		map_viewport:setOffset(map_viewport.offsetX,map_viewport.offsetY)
+	        end
+        end
+    end
+)
+
+map_layer:insertProp(debug_text)
